c14b615dbcc8c8c46303f30de609cbc5ea3cabd8
caiweiyjh <755804012@qq.com>
2018-05-28 14:12:52 +0800

    第一次提交

---
+++ src/java/Person.java
@@ -0,0 +1,60 @@
+package java;
+
+public class Person {
+    private  int id;
+    private  int age;
+    private  String name;
+    private  String job;
+
+    public Person() {
+    }
+
+    public Person(int id, int age, String name, String job) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.job = job;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("jackLove");
+        System.out.println("jackLove1");
+    }
+}
